187eba63fac22d0206012109d6ff8d2840ee9b12
Daniel Barros <daniel.barros@chartboost.com>
2022-10-14 17:22:28 +0200

    HB-4693: Adapt for new PartnerAdapter API

--- Source/YahooAdapter.swift
+++ Source/YahooAdapter.swift
@@ -10,159 +10,124 @@ import HeliumSdk
 import YahooAds
 import UIKit
 
-/// The Helium Yahoo adapter
-final class YahooAdapter: PartnerAdapter {    
-    /// Get the version of the Yahoo Mobile SDK.
+/// The Helium Yahoo adapter.
+final class YahooAdapter: PartnerAdapter {
+    
+    /// The version of the partner SDK, e.g. "5.13.2"
     let partnerSDKVersion = kYASSDKVersionKey
     
-    /// Get the version of the mediation adapter. To determine the version, use the following scheme to indicate compatibility:
-    /// [Helium SDK Major Version].[Partner SDK Major Version].[Partner SDK Minor Version].[Partner SDK Patch Version].[Adapter Version]
-    ///
-    /// For example, if this adapter is compatible with Helium SDK 4.x.y and partner SDK 1.0.0, and this is its initial release, then its version should be 4.1.0.0.0.
+    /// The version of the adapter, e.g. "2.5.13.2.0"
+    /// The first number is Helium SDK's major version. The next 3 numbers are the partner SDK version. The last number is the build version of the adapter.
     let adapterVersion = "4.1.1.0.0"
     
-    /// Get the internal name of the partner.
+    /// The partner's identifier.
     let partnerIdentifier = "yahoo"
     
-    /// Get the external/official name of the partner.
+    /// The partner's name in a human-friendly version.
     let partnerDisplayName = "Yahoo"
     
-    /// The key name for parsing the Yahoo Site ID
-    let siteIdKey = "site_id"
+    /// The designated initializer for the adapter.
+    /// Helium SDK will use this constructor to create instances of conforming types.
+    /// - parameter storage: An object that exposes storage managed by the Helium SDK to the adapter.
+    /// It includes a list of created `PartnerAd` instances. You may ignore this parameter if you don't need it.
+    init(storage: PartnerAdapterStorage) {}
     
-    /// Storage of adapter instances.  Keyed by the request identifier.
-    var adapters: [String: YahooAdAdapter] = [:]
-    
-    /// Override this method to initialize the Yahoo Mobile SDK so that it's ready to request and display ads.
-    /// - Parameters:
-    ///   - configuration: The necessary initialization data provided by Helium.
-    ///   - completion: Handler to notify Helium of task completion.
+    /// Does any setup needed before beginning to load ads.
+    /// - parameter configuration: Configuration data for the adapter to set up.
+    /// - parameter completion: Closure to be performed by the adapter when it's done setting up. It should include an error indicating the cause for failure or `nil` if the operation finished successfully.
     func setUp(with configuration: PartnerConfiguration, completion: @escaping (Error?) -> Void) {
         log(.setUpStarted)
         
-        guard let siteId = configuration.credentials[siteIdKey] as? String, !siteId.isEmpty else {
-            let error = self.error(.setUpFailure, description: "Site ID is null or empty.")
-            self.log(.setUpFailed(error))
-            
+        guard let siteId = configuration.siteID, !siteId.isEmpty else {
+            let error = error(.missingSetUpParameter(key: .siteIDKey))
+            log(.setUpFailed(error))
             completion(error)
             return
         }
         
         // Yahoo's initialization needs to be done on the main thread
         DispatchQueue.main.async {
-            let succeeded = YASAds.initialize(withSiteId:siteId)
+            let succeeded = YASAds.initialize(withSiteId: siteId)
             
             self.log(succeeded ? .setUpSucceded : .setUpFailed(self.error(.setUpFailure)))
             completion(succeeded ? nil : self.error(.setUpFailure))
         }
     }
     
-    /// Override this method to compute and return a bid token for the bid request.
-    /// - Parameters:
-    ///   - request: The necessary data associated with the current bid request.
-    ///   - completion: Handler to notify Helium of task completion.
-    func fetchBidderInformation(request: PreBidRequest, completion: @escaping ([String : String]) -> Void) {
-        log(.fetchBidderInfoStarted(request))
-        log(.fetchBidderInfoSucceeded(request))
-        
-        completion([:])
+    /// Fetches bidding tokens needed for the partner to participate in an auction.
+    /// - parameter request: Information about the ad load request.
+    /// - parameter completion: Closure to be performed with the fetched info.
+    func fetchBidderInformation(request: PreBidRequest, completion: @escaping ([String : String]?) -> Void) {
+        completion(nil)
     }
     
-    /// Override this method to notify your partner SDK of GDPR applicability as determined by the Helium SDK.
-    /// - Parameter applies: true if GDPR applies, false otherwise.
+    /// Indicates if GDPR applies or not.
+    /// - parameter applies: `true` if GDPR applies, `false` otherwise.
     func setGDPRApplies(_ applies: Bool) {
         if (applies) {
-            log(.privacyUpdated(setting: "GDPR", value: "applied"))
             YASAds.sharedInstance.applyGdpr()
+            log(.privacyUpdated(setting: "GDPR", value: "applied"))
         } else {
-            log("YASAds does not support setting applyGDPR to false")
+            // YASAds does not support setting applyGDPR to false
         }
     }
     
-    /// Override this method to notify your partner SDK of the GDPR consent status as determined by the Helium SDK.
-    /// - Parameter status: The user's current GDPR consent status.
+    /// Indicates the user's GDPR consent status.
+    /// - parameter status: One of the `GDPRConsentStatus` values depending on the user's preference.
     func setGDPRConsentStatus(_ status: GDPRConsentStatus) {
         // NO-OP as Helium does not support the TCF consent string.
-        log("Helium does not offer IAB Consent String support so YASAds will use its default value")
     }
     
-    /// Override this method to notify your partner SDK of the COPPA subjectivity as determined by the Helium SDK.
-    /// - Parameter isSubject: True if the user is subject to COPPA, false otherwise.
+    /// Indicates if the user is subject to COPPA or not.
+    /// - parameter isSubject: `true` if the user is subject, `false` otherwise.
     func setUserSubjectToCOPPA(_ isSubject: Bool) {
         if (isSubject) {
-            log(.privacyUpdated(setting: "COPPA", value: "applied"))
             YASAds.sharedInstance.applyCoppa()
+            log(.privacyUpdated(setting: "COPPA", value: "applied"))
         } else {
-            log("YASAds does not support setting applyCOPPA to false")
+            // YASAds does not support setting applyCOPPA to false
         }
     }
     
-    /// Override this method to notify your partner SDK of the CCPA privacy String as supplied by the Helium SDK.
-    /// - Parameters:
-    ///   - hasGivenConsent: True if the user has given CCPA consent, false otherwise.
-    ///   - privacyString: The CCPA privacy String.
+    /// Indicates the CCPA status both as a boolean and as a IAB US privacy string.
+    /// - parameter hasGivenConsent: A boolean indicating if the user has given consent.
+    /// - parameter privacyString: A IAB-compliant string indicating the CCPA status.
     func setCCPAConsent(hasGivenConsent: Bool, privacyString: String?) {
         guard let privacyString = privacyString, !privacyString.isEmpty else {
             log("Privacy string was empty, CCPA consent setting was not changed")
             return
         }
-        log(.privacyUpdated(setting: "YASCcpaConsent consentString", value: privacyString))
         YASAds.sharedInstance.add(YASCcpaConsent(consentString: privacyString))
+        log(.privacyUpdated(setting: "YASCcpaConsent", value: privacyString))
     }
     
-    /// Override this method to make an ad request to the partner SDK for the given ad format.
-    /// - Parameters:
-    ///   - request: The relevant data associated with the current ad load call.
-    ///   - partnerAdDelegate: Delegate for ad lifecycle notification purposes.
-    ///   - viewController: The ViewController for ad presentation purposes.
-    ///   - completion: Handler to notify Helium of task completion.
-    func load(request: PartnerAdLoadRequest, partnerAdDelegate: PartnerAdDelegate, viewController: UIViewController?,
-              completion: @escaping (Result<PartnerAd, Error>) -> Void) {
-        log(.loadStarted(request))
-        
-        /// Create and persist a new adapter instance
-        let adapter = YahooAdAdapter(adapter: self, request: request, partnerAdDelegate: partnerAdDelegate)
-        adapter.load(viewController: viewController, completion: completion)
-        
-        adapters[request.identifier] = adapter
-    }
-    
-    /// Override this method to show the currently loaded ad.
-    /// - Parameters:
-    ///   - partnerAd: The PartnerAd instance containing the ad to be shown.
-    ///   - viewController: The ViewController for ad presentation purposes.
-    ///   - completion: Handler to notify Helium of task completion.
-    func show(_ partnerAd: PartnerAd, viewController: UIViewController, completion: @escaping (Result<PartnerAd, Error>) -> Void) {
-        log(.showStarted(partnerAd))
-        
-        /// Retrieve the adapter instance to show the ad
-        if let adapter = adapters[partnerAd.request.identifier] {
-            adapter.show(viewController: viewController, completion: completion)
-        } else {
-            let error = error(.noAdReadyToShow(partnerAd))
-            log(.showFailed(partnerAd, error: error))
-            
-            completion(.failure(error))
+    /// Creates a new ad object in charge of communicating with a single partner SDK ad instance.
+    /// Helium SDK calls this method to create a new ad for each new load request. Ad instances are never reused.
+    /// Helium SDK takes care of storing and disposing of ad instances so you don't need to.
+    /// `invalidate()` is called on ads before disposing of them in case partners need to perform any custom logic before the object gets destroyed.
+    /// If for some reason a new ad cannot be provided an error should be thrown.
+    /// - parameter request: Information about the ad load request.
+    /// - parameter delegate: The delegate that will receive ad life-cycle notifications.
+    func makeAd(request: PartnerAdLoadRequest, delegate: PartnerAdDelegate) throws -> PartnerAd {
+        switch request.format {
+        case .interstitial, .rewarded:
+            return YahooAdapterFullscreenAd(adapter: self, request: request, delegate: delegate)
+        case .banner:
+            return YahooAdapterBannerAd(adapter: self, request: request, delegate: delegate)
         }
     }
-    
-    /// Override this method to discard current ad objects and release resources.
-    /// - Parameters:
-    ///   - partnerAd: The PartnerAd instance containing the ad to be invalidated.
-    ///   - completion: Handler to notify Helium of task completion.
-    func invalidate(_ partnerAd: PartnerAd, completion: @escaping (Result<PartnerAd, Error>) -> Void) {
-        log(.invalidateStarted(partnerAd))
-        
-        if adapters[partnerAd.request.identifier] != nil {
-            adapters.removeValue(forKey: partnerAd.request.identifier)
-            
-            log(.invalidateSucceeded(partnerAd))
-            completion(.success(partnerAd))
-        } else {
-            let error = error(.noAdToInvalidate(partnerAd))
-            
-            log(.invalidateFailed(partnerAd, error: error))
-            completion(.failure(error))
-        }
-    }
+}
+
+/// Convenience extension to access Yahoo credentials from the configuration.
+private extension PartnerConfiguration {
+    var siteID: String? { credentials[.siteIDKey] as? String }
+}
+
+extension String {
+    /// The key name for parsing the Yahoo Site ID
+    static let siteIDKey = "site_id"
+    /// The key name for the impression event.
+    static let impressionKey = "adImpression"
+    /// The key name for the video completion event.
+    static let videoCompletionKey = "onVideoComplete"
 }
---
+++ Source/YahooAdapterFullscreenAd.swift
@@ -0,0 +1,111 @@
+//
+//  YahooAdapterFullscreenAd.swift
+//  ChartboostHeliumAdapterYahoo
+//
+//  Created by Vu Chau on 9/13/22.
+//
+
+import Foundation
+import HeliumSdk
+import YahooAds
+
+/// The Helium Yahoo adapter fullscreen ad.
+final class YahooAdapterFullscreenAd: YahooAdapterAd, PartnerAd {
+    
+    /// The partner ad view to display inline. E.g. a banner view.
+    /// Should be nil for full-screen ads.
+    var inlineView: UIView? { nil }
+    
+    /// The Yahoo ad instance.
+    private var ad: YASInterstitialAd?
+    
+    /// Loads an ad.
+    /// - parameter viewController: The view controller on which the ad will be presented on. Needed on load for some banners.
+    /// - parameter completion: Closure to be performed once the ad has been loaded.
+    func load(with viewController: UIViewController?, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
+        log(.loadStarted)
+        
+        loadCompletion = completion
+        
+        let config = YASInterstitialPlacementConfig(placementId: request.partnerPlacement, requestMetadata: nil)
+        let ad = YASInterstitialAd(placementId: request.partnerPlacement)
+        self.ad = ad
+        ad.delegate = self
+        ad.load(with: config)
+    }
+    
+    /// Shows a loaded ad.
+    /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
+    /// - parameter viewController: The view controller on which the ad will be presented on.
+    /// - parameter completion: Closure to be performed once the ad has been shown.
+    func show(with viewController: UIViewController, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
+        log(.showStarted)
+        
+        if let ad = ad {
+            showCompletion = completion
+            ad.show(from: viewController)
+        } else {
+            let error = error(.noAdReadyToShow)
+            log(.showFailed(error))
+            completion(.failure(error))
+        }
+    }
+}
+
+// MARK: - YASInterstitialAdDelegate
+
+extension YahooAdapterFullscreenAd: YASInterstitialAdDelegate {
+    
+    func interstitialAdDidLoad(_ interstitialAd: YASInterstitialAd) {
+        log(.loadSucceeded)
+        loadCompletion?(.success([:])) ?? log(.loadResultIgnored)
+        loadCompletion = nil
+    }
+    
+    func interstitialAdLoadDidFail(_ interstitialAd: YASInterstitialAd, withError errorInfo: YASErrorInfo) {
+        let error = error(.loadFailure, error: errorInfo)
+        log(.loadFailed(error))
+        loadCompletion?(.failure(error)) ?? log(.loadResultIgnored)
+        loadCompletion = nil
+    }
+    
+    func interstitialAdDidFail(_ interstitialAd: YASInterstitialAd, withError errorInfo: YASErrorInfo) {
+        let error = error(.showFailure, error: errorInfo)
+        log(.showFailed(error))
+        showCompletion?(.failure(error)) ?? log(.showResultIgnored)
+        showCompletion = nil
+    }
+    
+    func interstitialAdDidShow(_ interstitialAd: YASInterstitialAd) {
+        log(.showSucceeded)
+        showCompletion?(.success([:])) ?? log(.showResultIgnored)
+        showCompletion = nil
+    }
+    
+    func interstitialAdDidClose(_ interstitialAd: YASInterstitialAd) {
+        log(.didDismiss(error: nil))
+        delegate?.didDismiss(self, details: [:], error: nil) ?? log(.delegateUnavailable)
+    }
+    
+    func interstitialAdClicked(_ interstitialAd: YASInterstitialAd) {
+        log(.didClick(error: nil))
+        delegate?.didClick(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+    
+    func interstitialAdDidLeaveApplication(_ interstitialAd: YASInterstitialAd) {
+        log("interstitialAdDidLeaveApplication")
+    }
+    
+    func interstitialAdEvent(_ interstitialAd: YASInterstitialAd, source: String, eventId: String, arguments: [String : Any]?) {
+        if (eventId == .impressionKey) {
+            log(.didTrackImpression)
+            delegate?.didTrackImpression(self, details: [:]) ?? log(.delegateUnavailable)
+        }
+        
+        if (eventId == .videoCompletionKey) {
+            let reward = Reward(amount: nil, label: nil)
+            log(.didReward(reward))
+            delegate?.didReward(self, details: [:], reward: reward) ?? log(.delegateUnavailable)
+        }
+    }
+}
