8be5199a79d1bdc1e1bc6038b713cbb688abc0b9
Daniel Barros <daniel.barros@chartboost.com>
2022-10-20 12:19:22 +0200

    Update comments

--- Source/YahooAdapter.swift
+++ Source/YahooAdapter.swift
@@ -17,7 +17,8 @@ final class YahooAdapter: PartnerAdapter {
     let partnerSDKVersion = kYASSDKVersionKey
     
     /// The version of the adapter.
-    /// The first digit is Helium SDK's major version. The last digit is the build version of the adapter. The intermediate digits correspond to the partner SDK version.
+    /// It should have 6 digits separated by periods, where the first digit is Helium SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
+    /// Format: `"<Helium major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>"`.
     let adapterVersion = "4.1.1.0.0"
     
     /// The partner's unique identifier.
